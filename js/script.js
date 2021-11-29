934470519f50d03cd7a8b82ccb300cecc7021901
Lautaro Coria <lautaronahuel.coria@gmail.com>
2021-11-29 17:02:09 -0300

    Se agregó sección sobre mí

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-const textTrainee = document.querySelector('#trainee-it');
-let mensaje = '| Trainee it';
+// const textTrainee = document.querySelector('#trainee-it');
+// let mensaje = '| Trainee it';
+// let i = 1;
+
+// window.document.addEventListener('DOMContentLoaded', setInterval('mostrarLetra()', 100));
+
+// function mostrarLetra() {
+//     if(mensaje.length+1 === i){
+//         return;
+//     }else{
+//         textTrainee.textContent = mensaje.slice(0,i++);
+//     }
+
+// }
+
+const textWelcome = document.querySelector('#welcome');
+let mensaje = 'Welcome to my website.';
 let i = 1;
 
 window.document.addEventListener('DOMContentLoaded', setInterval('mostrarLetra()', 100));
@@ -7,8 +22,7 @@ window.document.addEventListener('DOMContentLoaded', setInterval('mostrarLetra()
 function mostrarLetra() {
     if(mensaje.length+1 === i){
         return;
-    }else{
-        textTrainee.textContent = mensaje.slice(0,i++);
+    }else {
+        textWelcome.innerHTML = mensaje.slice(0,i++);
     }
-
 }
