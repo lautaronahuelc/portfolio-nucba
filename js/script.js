fa4cdef26e40d51030aa71ba93b35d2fb2d25a20
Lautaro Coria <lautaronahuel.coria@gmail.com>
2021-12-02 00:31:59 -0300

    Se le agregó evento a los enlaces

--- js/script.js
+++ js/script.js
@@ -1,24 +1,17 @@
-// const textTrainee = document.querySelector('#trainee-it');
-// let mensaje = '| Trainee it';
-// let i = 1;
-
-// window.document.addEventListener('DOMContentLoaded', setInterval('mostrarLetra()', 100));
-
-// function mostrarLetra() {
-//     if(mensaje.length+1 === i){
-//         return;
-//     }else{
-//         textTrainee.textContent = mensaje.slice(0,i++);
-//     }
-
-// }
-
+// Variables para hacer la animación del hero
 const textWelcome = document.querySelector('#welcome');
 let mensaje = 'Welcome to my website.';
 let i = 1;
 
-window.document.addEventListener('DOMContentLoaded', setInterval('mostrarLetra()', 100));
+// Evento para el hero
+window.addEventListener('DOMContentLoaded', iniciarIntervalo);
 
+// Llama al método setInverval
+function iniciarIntervalo() {
+    setInterval('mostrarLetra()', 100);
+}
+
+// Se imprimen la letras una por una en el hero
 function mostrarLetra() {
     if(mensaje.length+1 === i){
         return;
@@ -26,3 +19,31 @@ function mostrarLetra() {
         textWelcome.innerHTML = mensaje.slice(0,i++);
     }
 }
+
+// Evento scroll para los enlaces
+window.addEventListener('scroll', () => {
+    if (document.documentElement.clientWidth <= 730) {
+        const buttonSobreMi = document.querySelector('#nav-s');
+        const seccionSobreMi = document.querySelector('#sobre-mi');
+        const ubicacionSobreMi = seccionSobreMi.getBoundingClientRect();
+
+        const buttonHerramintas = document.querySelector('#nav-h');
+        const seccionHerramientas = document.querySelector('#herramientas');
+        const ubicacionHerramientas = seccionHerramientas.getBoundingClientRect();
+
+        if(ubicacionSobreMi.bottom > 150){
+            buttonSobreMi.classList.add('selected');
+        } else {
+            buttonSobreMi.classList.remove('selected');
+        }
+
+
+        if(ubicacionSobreMi.bottom < 150 && ubicacionHerramientas.bottom < 846) {
+            buttonHerramintas.classList.add('selected');
+        } else {
+            buttonHerramintas.classList.remove('selected');
+        }
+    }
+});
+
+
